b3fce100a01a6f530011f3df4934455ce53ab19d
Minhthuyy <minhthuyy16@gmail.com>
2026-04-04 00:24:08 +0700

    Tinh Ke Thua P1

---
+++ JavaOOP012026/src/com/anhtester/TinhKeThua/Student.java
@@ -0,0 +1,10 @@
+package com.anhtester.TinhKeThua;
+
+public class Student extends Person {
+
+    public Student(String name, int age, float height) {
+        super(name, age, height); //Gọi constructor của lớp cha (Person)
+
+    }
+
+}
