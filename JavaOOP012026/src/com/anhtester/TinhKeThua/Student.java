db6d2f981815d43e7dfe23ba62fe48736717bf08
Minhthuyy <minhthuyy16@gmail.com>
2026-04-04 14:11:52 +0700

    Tinh Ke Thua P2

--- JavaOOP012026/src/com/anhtester/TinhKeThua/Student.java
+++ JavaOOP012026/src/com/anhtester/TinhKeThua/Student.java
@@ -2,9 +2,35 @@ package com.anhtester.TinhKeThua;
 
 public class Student extends Person {
 
-    public Student(String name, int age, float height) {
-        super(name, age, height); //Gọi constructor của lớp cha (Person)
+    private String address;
+    private float weight;
 
+    public Student(String name, int age, String address, float height, float weight) {
+        super(name, age, height); //Gọi constructor của lớp cha (Person)
     }
+
+
+    //2 hàm này của class Student
+    public String getAddress(){
+        return address;
+    }
+
+    public Float getWeight(){
+        return weight;
+    }
+
+    public void testGetInfoOfStudent() {
+        Person person = new Person("Anhtester", 30, 1.75f);
+        person.getInfo();
+
+        getInfo(); //Gọi phương thức của lớp cha (Person)
+    }
+
+
+    public void testTinhDiemTrungBinh(){
+        getInfo();
+    }
+
+
 
 }
